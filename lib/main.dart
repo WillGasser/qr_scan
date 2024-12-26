de8b4ee4d4f65ddd8bd598117702c2c46d01290e
unknown <willbg04@gmail.com>
2024-12-26 05:03:43 -0600

    Adding states and nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
@@ -6,8 +9,28 @@ void main() {
   runApp(const MyApp());
 }
 
-// App State Management
-class MyAppState extends ChangeNotifier {
+// Root Widget
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => _MyAppState(), // Provide app state
+      child: MaterialApp(
+        title: 'QR Scanner',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          scaffoldBackgroundColor:
+              const Color(0xFFF8F8F8), // Off-white background
+        ),
+        home: const LoadingScreen(), // Start with Loading Screen
+      ),
+    );
+  }
+}
+
+class _MyAppState extends ChangeNotifier {
   String qrData =
       'https://www.youtube.com/embed/LWNk7oJRbi0?start=33&autoplay=1';
 
@@ -18,22 +41,54 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-// Root Widget
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+//Loading Screen
+class LoadingScreen extends StatefulWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 3), // 2 seconds animation
+      vsync: this,
+    );
+
+    _animation = Tween<double>(begin: 1.0, end: 0.0).animate(_controller);
+
+    Future.delayed(const Duration(seconds: 1), () {
+      _controller.forward().then((value) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const MyHomePage()),
+        );
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Cleanup controller
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(), // Provide app state
-      child: MaterialApp(
-        title: 'QR Scanner',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-          scaffoldBackgroundColor:
-              const Color(0xFFF8F8F8), // Off-white background
+    return Scaffold(
+      body: Center(
+        child: FadeTransition(
+          opacity: _animation, // Connect animation to FadeTransition
+          child: const CircularProgressIndicator(),
         ),
-        home: const MyHomePage(),
       ),
     );
   }
@@ -45,41 +100,90 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
+    var appState = context.watch<_MyAppState>();
+
+    // Get the screen width dynamically
+    double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(35.0),
+        padding:
+            EdgeInsets.all(screenWidth * 0.1), // 10% of screen width as padding
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
+            // Dynamic Title
+            Text(
               'QR Scanner Example',
               style: TextStyle(
                 fontFamily: 'Times New Roman',
-                fontSize: 45.0,
+                fontSize: screenWidth * 0.07, // 7% of screen width
                 fontWeight: FontWeight.bold,
               ),
             ),
-            // QR Code Display
+            const SizedBox(height: 20), // Spacer
+
+            // Dynamic QR Code Display
             QrImageView(
               data: appState.qrData,
               version: QrVersions.auto,
-              size: 500.0,
+              size: screenWidth * 0.6, // 60% of screen width
             ),
             const SizedBox(height: 20), // Spacer
 
-            // Elevated Button to Update QR Code
-            ElevatedButton(
-              onPressed: () {
-                // Update the QR code with new data
-                appState.updateQR(
-                    'https://www.youtube.com/watch?v=9I-2rAxBd-4&t=834s&ab_channel=B9poy');
-              },
-              child: const Text('Generate New QR Code'),
+            // Dynamic Button
+            SizedBox(
+              width: screenWidth * 0.5, // 50% of screen width
+              height: screenWidth * 0.1, // 10% of screen width
+              child: ElevatedButton(
+                onPressed: () {
+                  // Update the QR code with new data
+                  appState.updateQR(
+                    'https://www.youtube.com/watch?v=9I-2rAxBd-4&t=834s&ab_channel=B9poy',
+                  );
+                },
+                child: Text(
+                  'Generate New QR Code',
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.04, // 4% of screen width
+                  ),
+                ),
+              ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+// Navigation Bar
+class NavigationBar extends StatefulWidget {
+  const NavigationBar({super.key});
+
+  @override
+  State<NavigationBar> createState() => _NavigationBarState();
+}
+
+class _NavigationBarState extends State<NavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QR_Code'), // Title
+        actions: [
+          IconButton(
+            icon: Image.asset('assets/icon1.png'), // Image button 1
+            onPressed: () {
+            },
+          ),
+          IconButton(
+            icon: Image.asset('assets/icon2.png'), // Image button 2
+            onPressed: () {
+              Link
+            },
+          ),
+        ],
       ),
     );
   }
